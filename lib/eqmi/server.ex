316ef5c98002f49080c419afbad73a665972702d
nadsat <antonio.martinez.c@protonmail.com>
2021-02-11 22:03:41 -0300

    add gen server basic

---
+++ lib/eqmi/server.ex
@@ -0,0 +1,14 @@
+defmodule Eqmi.Server do
+  def start_link(dev) do
+    GenServer.start_link(__MODULE__, dev, __MODULE__)
+  end
+
+  def init(dev) do
+    options = [:read, :write, :raw]
+
+    case File.open(dev, options) do
+      {:ok, dev} -> {:ok, %{device: dev}}
+      {:error, reason} -> {:stop, reason}
+    end
+  end
+end
